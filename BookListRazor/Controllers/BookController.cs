bd1801163b685660a61dd86f0f334ac613f11fb7
Barış Can <bariscp@gmail.com>
2021-08-23 23:06:07 +0300

    First Commit

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Book.ToList() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var record = await _db.Book.FindAsync(id);
+            if(record is null)
+                return Json( new {success=false,message="Error while deleting"});
+
+            _db.Book.Remove(record);
+
+            await _db.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Delete successful" });
+        }
+    }
+}
