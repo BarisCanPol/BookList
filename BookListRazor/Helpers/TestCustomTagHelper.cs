b800cf1a61fae3071db44dd591f494b3925573c0
Barış Can <bariscp@gmail.com>
2021-09-06 23:24:48 +0300

    Added Simple Custom Tag Helper

---
+++ BookListRazor/Helpers/TestCustomTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Utilities.CustomTagHelper
+{
+    [HtmlTargetElement("test-custom-tag-helper")]
+    public class TestCustomTagHelper : TagHelper 
+    {
+        public string Name { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "TestCustomTagHelper";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("<span>Hi! {0}</span>", this.Name);
+
+            output.PreContent.SetHtmlContent(sb.ToString());
+        }
+    }
+}
